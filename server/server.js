5a8562e872b038394979e46483dce1baddebddbd
Santiago Rendon Valencia <srendonv@unal.edu.co>
2022-04-06 08:01:17 -0500

    cleaning and docs

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const cors = require('cors');
 // get MongoDB driver connection
 const dbo = require('./db/conn');
+const {seedDB} = require('./db/seedDB');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -25,6 +26,9 @@ dbo.connectToServer(function (err) {
     console.error(err);
     process.exit();
   }
+
+  // Fill database with data
+  seedDB();
 
   // start the Express server
   app.listen(PORT, () => {
